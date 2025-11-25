60551e7b65def6654155b2eb268b0e8ea61ee23b
Matthew Do <qvs82v2vm@mozmail.com>
2025-11-25 09:37:44 -0500

    more reliable current branch detection

--- src/command/done.rs
+++ src/command/done.rs
@@ -1,19 +1,14 @@
 use crate::command::common;
-use anyhow::{anyhow, Result};
-
-const CURRENT_BRANCH_PREFIX: char = '*';
+use anyhow::Result;
 
 pub fn done<DefaultBranch: AsRef<str>>(default_branch: DefaultBranch) -> Result<()> {
     common::ensure_working_directory_is_clean()?;
 
-    let git_branch_output = String::from_utf8(common::execute("git", &["branch"])?)?;
-    let current_branch = git_branch_output
-        .split('\n')
-        .find(|line| line.starts_with(CURRENT_BRANCH_PREFIX))
-        .ok_or(anyhow!("failed to find current branch"))?
-        .strip_prefix(CURRENT_BRANCH_PREFIX)
-        .ok_or(anyhow!("failed to strip current branch prefix"))?
-        .trim();
+    let current_branch_output = String::from_utf8(common::execute(
+        "git",
+        &["rev-parse", "--abbrev-ref", "HEAD"],
+    )?)?;
+    let current_branch = current_branch_output.trim();
 
     common::execute("git", &["checkout", default_branch.as_ref()])?;
     common::execute("git", &["branch", "-D", current_branch])?;
